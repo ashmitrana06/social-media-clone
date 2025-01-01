e6bcc443321cb419fdaa7916732e95155a2d3008
ashmitrana06 <167062968+ashmitrana06@users.noreply.github.com>
2025-01-02 03:58:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "block";
+  }
+  
+  function closePopup() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "none";
+  }
