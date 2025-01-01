ab326a947794b208bd86579251cbcb3282da863e
ashmitrana06 <167062968+ashmitrana06@users.noreply.github.com>
2025-01-02 03:59:44 +0530

    Add files via upload

---
+++ explore/explore.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "block";
+  }
+  
+  function closePopup() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "none";
+  }
+
+  
